c6d6491e27278f104843a909480738fb0976442b
Brynja Palina Sigurgeirsdottir <binnapalla@gmail.com>
2024-03-18 20:08:29 +0000

    added bootstrap and partial ejs files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // using ejs to template
 app.set('view engine', 'ejs');
 // makes it possible for index.js file to find views folder whereever it is located
